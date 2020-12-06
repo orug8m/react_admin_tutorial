0a7438c5809bf08dbafa0f43ec117a409ba997cd
orug8m <licht7xinoza1@gmail.com>
2020-12-06 22:45:45 +0900

    complete except changing dataProvider

---
+++ src/users.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { List, Datagrid, TextField, EmailField  } from 'react-admin'
+import MyUrlField from './components/MyUrlField';
+
+export const UserList: React.FC = props => {
+    return (
+        <List {...props}>
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <TextField source="name" />
+                <EmailField source="email" />
+                <TextField source="phone" />
+                <MyUrlField source="website" />
+                <TextField source="company.name" />
+            </Datagrid>
+        </List>
+    )
+
+}
